f6f560d2f72c58bd203a84cf9cac7fe33631db44
imeuro <mauro.fioravanzi@gmail.com>
2024-11-11 17:30:36 +0100

    fix selettore lingua in agenda

--- wp-content/themes/aai/sidebar.php
+++ wp-content/themes/aai/sidebar.php
@@ -19,20 +19,32 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 		<section id="polylang-selector" class="widget widget_polylang">
 			<ul>
 				<?php
-				$languages = pll_the_languages( array( 'raw' => 1 ) );
-				if ( ! empty( $languages ) ) {
-					foreach ( $languages as $language ) { 
-						// echo '<pre>';
-						// print_r($language);
-						// echo '</pre>';
-						if ( ! $language['current_lang'] ) {
-							$translation_post_id = pll_get_post(get_the_ID(), $language['slug']);
-							$translation_post_url = get_permalink($translation_post_id);
-							echo '<li class="lang-item lang-item-' . $language['slug'] . '"><a lang="' . $language['locale'] .'" hreflang="' . $language['locale'] .'" href="' . esc_url( $translation_post_url ) . '">' . esc_html( $language['name'] ) . '</a></li>';
-						} else {
-							echo '<li class="lang-item lang-item-' . $language['slug'] . '"><span>' . esc_html( $language['name'] ) . '</span></li>';}
+				if ( is_post_type_archive( 'agenda' ) ) { 
+					$language = (isset($_GET['lang'])) ? $_GET['lang'] : 'it';
+					if ($language == 'it') {
+						echo '<li class="lang-item lang-item-it"><span>ITA</span></li>';
+						echo '<li class="lang-item lang-item-en"><a lang="en-GB" hreflang="en-GB" href="'.esc_url( home_url( '/agenda/?lang=en' ) ).'">ENG</a></li>';
+					} else {
+						echo '<li class="lang-item lang-item-it"><a lang="it-IT" hreflang="it-IT" href="'.esc_url( home_url( '/agenda/?lang=it' ) ).'">ITA</a></li>';
+						echo '<li class="lang-item lang-item-en"><span>ENG</span></li>';
 					}
-				} 
+				} else { 
+					$languages = pll_the_languages( array( 'raw' => 1 ) );
+					if ( ! empty( $languages ) ) {
+						foreach ( $languages as $language ) { 
+							// echo '<pre>';
+							// print_r($language);
+							// echo '</pre>';
+							if ( ! $language['current_lang'] ) {
+								$translation_post_id = pll_get_post(get_the_ID(), $language['slug']);
+								$translation_post_url = get_permalink($translation_post_id);
+
+								echo '<li class="lang-item lang-item-' . $language['slug'] . '"><a lang="' . $language['locale'] .'" hreflang="' . $language['locale'] .'" href="' . esc_url( $translation_post_url ) . '">' . esc_html( $language['name'] ) . '</a></li>';
+							} else {
+								echo '<li class="lang-item lang-item-' . $language['slug'] . '"><span>' . esc_html( $language['name'] ) . '</span></li>';}
+						}
+					} 
+				}
 				?>
 			</ul>
 		</section>
