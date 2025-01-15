a4e4ccdbcf064618b0fb1f9c7d0bbbb480d06e22
imeuro <mauro.fioravanzi@gmail.com>
2025-01-15 19:06:08 +0100

    foto a fondo pagina per articoli vecchi + excerpt biblioteca

--- wp-content/themes/aai/template-parts/content-archive.php
+++ wp-content/themes/aai/template-parts/content-archive.php
@@ -9,13 +9,17 @@
 
 global $paged, $postCount, $style, $lastPostYear; 
 $posteven = $postCount % 2 ? 'post-even' : 'post-odd';
+if ( get_post_type() == 'biblioteca' ) {
+  $excerpt = wp_trim_words( get_the_content(), 50 );
+} else { $excerpt = get_the_excerpt(); }
+
 ?>
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(array('HP-item-opening',$posteven)); ?> data-year="<?php echo get_the_date("Y"); ?>">
 		<a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
 			<header class="entry-header">
 				<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
-				<p class="entry-excerpt"><?php echo  get_the_excerpt(); ?></p> 
+				<p class="entry-excerpt"><?php echo $excerpt; ?></p> 
 				<?php if (get_field('aai_post_author')) {
 					echo "<p class=\"entry-auth\">di ". get_field('aai_post_author') ."</p>";
 				} ?>
